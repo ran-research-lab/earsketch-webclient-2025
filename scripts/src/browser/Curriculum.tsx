590d5838ef4f4d627dc7e86dc6d659225dad1a5c
Ian Clester <ijc@ijc8.me>
2021-07-01 08:52:50 -0400

    Fix curriculum loading spinner. (#193)

--- scripts/src/browser/Curriculum.tsx
+++ scripts/src/browser/Curriculum.tsx
@@ -252,7 +252,7 @@ const CurriculumPane = () => {
             <div id="curriculum" className={theme === 'light' ? 'curriculum-light' : 'dark'} style={{fontSize}}>
                 {content ? 
                   <article ref={curriculumBody} id="curriculum-body" className="prose dark:prose-dark px-8 h-full max-w-none overflow-y-auto" style={{fontSize}} />
-                : <div>
+                : <div className="flex flex-col items-center">
                       <div className="text-4xl text-center py-16">Loading curriculum...</div>
                       <div className="loading-spinner" style={{width: '90px', height: '90px', borderWidth: '9px'}} />
                   </div>}
