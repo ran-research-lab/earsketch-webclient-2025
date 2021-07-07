ed1329697ea8f8d58d5d17f6a45f7607ba4d4f17
Ian Clester <ijc@ijc8.me>
2021-07-07 16:04:13 -0400

    Tweak comma-dangle, no-use-before-define rules. (#199)
    * Tweak comma-dangle, no-use-before-define rules.
    
    * Add rule: includes() rather than indexOf() > -1.
    
    * Add rule: type annotation spacing.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,23 @@ module.exports = {
     ],
     rules: {
         "no-use-before-define": "off",
-        "@typescript-eslint/no-use-before-define": ["error"],
+        "@typescript-eslint/no-use-before-define": ["error", { functions: false, variables: false, classes: false }],
+        "@typescript-eslint/type-annotation-spacing": ["error"],
         "space-before-function-paren": ["error", { named: "never" }],
-        "comma-dangle": ["error", "always-multiline"],
+        "comma-dangle": ["error", {
+            arrays: "always-multiline",
+            objects: "always-multiline",
+            imports: "always-multiline",
+            exports: "always-multiline",
+            functions: "never",
+        }],
         indent: ["error", 4],
         quotes: ["error", "double", { avoidEscape: true }],
         "jsx-quotes": ["error", "prefer-double"],
+        "no-restricted-syntax": ["error", {
+            selector: "BinaryExpression > CallExpression > MemberExpression > Identifier[name = 'indexOf']",
+            message: "Use .includes() instead of comparing the result of .indexOf()",
+        }],
     },
     overrides: [
         {
