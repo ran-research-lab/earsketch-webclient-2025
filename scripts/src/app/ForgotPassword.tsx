2db393a0ef90fa164386a2c63ce86d8a9cbcce79
Ian Clester <ijc@ijc8.me>
2021-07-21 15:36:42 -0400

    Update ForgotPassword to use endpoint correctly. (#225)
    Fixes GTCMT/earsketch#2569.

--- scripts/src/app/ForgotPassword.tsx
+++ scripts/src/app/ForgotPassword.tsx
@@ -3,38 +3,36 @@ import React, { useState } from "react"
 import esconsole from "../esconsole"
 import * as userNotification from "../user/notification"
 import { useTranslation } from "react-i18next"
+import { post } from "./userProject"
 
 export const ForgotPassword = ({ close }: { close: () => void }) => {
-    const { t } = useTranslation();
+    const { t } = useTranslation()
     const [email, setEmail] = useState("")
 
     const resetPassword = () => {
-        const url = URL_DOMAIN + '/users/resetpwd?email=' + encodeURIComponent(email)
-        fetch(url).then(response => {
-            if (response.ok) {
-                esconsole("Forgot Password succeeded", "info")
-                userNotification.show(t('messages:forgotpassword.success'), "success", 3.5)
-            } else {
-                esconsole("Forgot Password failed", "info")
-                userNotification.show(t('messages:forgotpassword.fail'), "failure1", 3.5)
-            }
+        post("/users/resetpwd", { email }).then(() => {
+            esconsole("Forgot Password succeeded", "info")
+            userNotification.show(t("messages:forgotpassword.success"), "success", 3.5)
+        }).catch(() => {
+            esconsole("Forgot Password failed", "info")
+            userNotification.show(t("messages:forgotpassword.fail"), "failure1", 3.5)
         })
         close()
     }
 
     return <>
         <div className="modal-header">
-            <h3>{t('forgotPassword.title')}</h3>
+            <h3>{t("forgotPassword.title")}</h3>
         </div>
         <form onSubmit={e => { e.preventDefault(); resetPassword() }}>
             <div className="modal-body">
                 <label className="w-full">
-                    {t('forgotPassword.prompt')}
-                    <input type="email" className="form-control" name="email" placeholder={t('forgotPassword.email')} required autoComplete="off" value={email} onChange={e => setEmail(e.target.value)} />
+                    {t("forgotPassword.prompt")}
+                    <input type="email" className="form-control" name="email" placeholder={t("forgotPassword.email")} required autoComplete="off" value={email} onChange={e => setEmail(e.target.value)} />
                 </label>
             </div>
             <div className="modal-footer">
-                <input type="submit" value={t('forgotPassword.submit') as string} className="btn btn-primary" />
+                <input type="submit" value={t("forgotPassword.submit") as string} className="btn btn-primary" />
             </div>
         </form>
     </>
