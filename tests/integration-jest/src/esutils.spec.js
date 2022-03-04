909960b953990cd98eaa8b7b51f3ac142c9e9856
Jason Smith <jas1238smith@gmail.com>
2022-03-04 11:51:55 -0500

    Test: esutils parseName (#348)
    * update esutils.spec.js
    
    * use test.each() format
    
    Co-authored-by: heerman <lwheerman@gmail.com>

--- tests/integration-jest/src/esutils.spec.js
+++ tests/integration-jest/src/esutils.spec.js
@@ -1,14 +1,6 @@
 /* eslint-env jest */
 import * as esutils from "../../../scripts/src/esutils"
 
-it("detects py with parseLanguage()", () => {
-    expect(esutils.parseLanguage("song.py")).toBe("python")
-})
-
-it("detects js with parseLanguage()", () => {
-    expect(esutils.parseLanguage("song.js")).toBe("javascript")
-})
-
 test.each([
     { measure: 2, tempo: -1, timeSignature: 4, expected: 2.0 },
     { measure: 2, tempo: 120, timeSignature: 4, expected: 2.0 },
@@ -16,6 +8,21 @@ test.each([
     { measure: 3, tempo: 220, timeSignature: 4, expected: 2.18181818 },
     { measure: 9.25, tempo: 88, timeSignature: 4, expected: 22.5 },
     { measure: 10, tempo: 110, timeSignature: 3, expected: 14.727272727 },
-])(".measureToTime($measure, $tempo, timeSignature)", ({ measure, tempo, timeSignature, expected }) => {
+])("esutils measureToTime($measure, $tempo, timeSignature)", ({ measure, tempo, timeSignature, expected }) => {
     expect(esutils.measureToTime(measure, tempo, timeSignature)).toBeCloseTo(expected, 8)
+})
+
+test.each([
+    { filename: "song.py", expected: "python" },
+    { filename: "song.js", expected: "javascript" },
+])("esutils parseLanguage($filename)", ({ filename, expected }) => {
+    expect(esutils.parseLanguage(filename)).toBe(expected)
+})
+
+test.each([
+    { filename: "test.abc", expected: "test" },
+    { filename: "test", expected: "test" },
+    { filename: "test.test.abc", expected: "test.test" },
+])("esutils parseName($filename)", ({ filename, expected }) => {
+    expect(esutils.parseName(filename)).toBe(expected)
 })
