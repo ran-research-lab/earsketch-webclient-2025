016ebfd66cc6d9cff17cdd9ba6f3a668345d4be8
Ashvala Vinay <Ashvala@users.noreply.github.com>
2022-03-04 12:52:25 -0500

    Tests: compareObjStructure (#352)
    * Create unit tests for compareObjStructure
    
    * use test.each()
    
    Co-authored-by: heerman <lwheerman@gmail.com>

--- tests/integration-jest/src/esutils.spec.js
+++ tests/integration-jest/src/esutils.spec.js
@@ -8,14 +8,14 @@ test.each([
     { measure: 3, tempo: 220, timeSignature: 4, expected: 2.18181818 },
     { measure: 9.25, tempo: 88, timeSignature: 4, expected: 22.5 },
     { measure: 10, tempo: 110, timeSignature: 3, expected: 14.727272727 },
-])("esutils measureToTime($measure, $tempo, timeSignature)", ({ measure, tempo, timeSignature, expected }) => {
+])("measureToTime($measure, $tempo, timeSignature)", ({ measure, tempo, timeSignature, expected }) => {
     expect(esutils.measureToTime(measure, tempo, timeSignature)).toBeCloseTo(expected, 8)
 })
 
 test.each([
     { filename: "song.py", expected: "python" },
     { filename: "song.js", expected: "javascript" },
-])("esutils parseLanguage($filename)", ({ filename, expected }) => {
+])("parseLanguage($filename)", ({ filename, expected }) => {
     expect(esutils.parseLanguage(filename)).toBe(expected)
 })
 
@@ -23,20 +23,31 @@ test.each([
     { filename: "test.abc", expected: "test" },
     { filename: "test", expected: "test" },
     { filename: "test.test.abc", expected: "test.test" },
-])("esutils parseName($filename)", ({ filename, expected }) => {
+])("parseName($filename)", ({ filename, expected }) => {
     expect(esutils.parseName(filename)).toBe(expected)
 })
 
 test.each([
     { value: 0.123456789, expected: 0.12346 },
     { value: 0.123, expected: 0.12300 },
-])("esutils toPrecision($value)", ({ value, expected }) => {
+])("toPrecision($value)", ({ value, expected }) => {
     expect(esutils.toPrecision(value)).toBe(expected)
 })
 
 test.each([
     { value: 0.123456789, digits: 5, expected: 0.12346 },
     { value: 0.123, digits: 8, expected: 0.12300000 },
-])("esutils toPrecision($value, $digits)", ({ value, expected }) => {
+])("toPrecision($value, $digits)", ({ value, expected }) => {
     expect(esutils.toPrecision(value)).toBe(expected)
+})
+
+test.each([
+    { desc: "empty objects", expected: true, objA: {}, objB: {} },
+    { desc: "identical objects", expected: true, objA: { a: 1, b: 2, c: 3 }, objB: { a: 1, b: 2, c: 3 } },
+    { desc: "dissimilar objects", expected: false, objA: { a: 1, b: 2, c: 3 }, objB: { a: 1, b: 2 } },
+    { desc: "same props diff values", expected: true, objA: { a: 1, b: 2, c: 3 }, objB: { a: 1, b: 2, c: 4 } },
+    { desc: "nested objects", expected: true, objA: { a: 1, b: 2, c: { d: 3, e: 4 } }, objB: { a: 1, b: 2, c: { d: 3, e: 4 } } },
+    { desc: "dissimilar nested", expected: false, objA: { a: 1, b: 2, c: { d: 3, e: 4 } }, objB: { a: 1, b: 2, c: { d: 3 } } },
+])("compareObjStructure($desc)", ({ objA, objB, expected }) => {
+    expect(esutils.compareObjStructure(objA, objB)).toBe(expected)
 })
