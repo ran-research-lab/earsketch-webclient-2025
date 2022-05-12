99b6c567d932d4b938e8fee1a3ab549c2bd4c857
heerman <43624284+heerman@users.noreply.github.com>
2022-05-12 17:24:28 -0400

    Cypress test: DAW (#385)
    * play and pause the daw
    
    * push all daw buttons
    
    * combine script py test and daw test
    
    Co-authored-by: Stephen Garrett <manodrum@gmail.com>

--- tests/e2e-cypress/integration/daw.spec.js
+++ tests/e2e-cypress/integration/daw.spec.js
@@ -1,8 +1,7 @@
 import * as MockSocket from "mock-socket"
 
-describe("fitMedia (py) script", () => {
-    it("Logs in, creates a new script, runs with fit media", () => {
-        // Stubbing
+describe("DAW", () => {
+    it("Runs script, playback in DAW", () => {
         const testSoundMeta = {
             artist: "RICHARD DEVINE",
             folder: "DUBSTEP_140_BPM__DUBBASSWOBBLE",
@@ -24,7 +23,7 @@ describe("fitMedia (py) script", () => {
             run_status: 1,
             shareid: "qeT7pez_OVHwmxeDVzkT7w",
             soft_delete: false,
-            source_code: "from earsketch import *\n\ninit()\nsetTempo(120)\nfitMedia(DUBSTEP_BASS_WOBBLE_002, 1, 1, 5)\n\nfinish()\n",
+            source_code: "from earsketch import *\n\ninit()\nsetTempo(120)\nfitMedia(DUBSTEP_BASS_WOBBLE_002, 1, 1, 1.5)\n\nfinish()\n",
             username: "cypress",
         }
 
@@ -44,16 +43,38 @@ describe("fitMedia (py) script", () => {
         cy.visitWithStubWebSocket("/", MockSocket.WebSocket)
         cy.login()
 
-        cy.get("#app-title").should("contain", "EarSketch")
-
-        // Load Script
+        // Load py script
         cy.get("button[title='Open SCRIPTS Tab']").click()
         cy.contains("div", "playsound.py").click()
 
-        // Run new Script
+        // Run script
         cy.get("button[id='run-button']").click()
 
         // Confirm success from the ES console
         cy.contains("span", "Script ran successfully")
+
+        // Verify DAW has track "1" and "Play" is showing
+        cy.contains(".dawTrackName", "1") // indicates track "1" rendered
+        cy.get(".dawAudioClipContainer") // indicates timeline waveform rendered
+        cy.get("button[title='Play']")
+
+        // Play, loop, rewind, metronome in DAW
+        cy.get("button[title='Toggle Metronome']").click()
+        cy.get("button[title='Reset']").click()
+        cy.get("button[title='Loop Project']").click() // enable loop
+
+        // We could cy.wait(SONG_LENGTH) here to confirm looping, but I'd rather
+        // avoid using wait in this way. The best validation I found was to
+        // confirm audio playback with the play/pause button.
+
+        // Verify "Play" becomes "Pause" on click
+        cy.get("button[title='Play']").click()
+        cy.get("button[title='Play']").should("not.exist")
+        cy.get("button[title='Pause']")
+
+        // Verify "Pause" becomes play when song playback ends
+        cy.get("button[title='Pause']").click()
+        cy.get("button[title='Pause']").should("not.exist")
+        cy.get("button[title='Play']")
     })
 })
