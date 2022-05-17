cb778c5b8054562ea15457d6f10828a85ce666aa
heerman <43624284+heerman@users.noreply.github.com>
2022-05-17 15:05:09 -0400

    Cypress test: language selector (#391)
    * test language selector
    
    * don't disable eslint
    
    * change search text
    
    only the abbreviated DAW is visible at the resolution cypress is testing at. The other languages are searching for the Content Manager text, so let's use that for consistency.
    
    * use `h2` tags for more precision
    
    Co-authored-by: Stephen Garrett <manodrum@gmail.com>

---
+++ tests/e2e-cypress/integration/internationalization.spec.js
@@ -0,0 +1,22 @@
+describe("language", () => {
+    it("selects language", () => {
+        cy.interceptAudioStandard()
+        cy.visit("/")
+        cy.get("button").contains("Skip").click()
+
+        // select spanish
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "Español").click()
+        cy.contains("h2", "GESTOR DE CONTENIDOS")
+
+        // select french
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "Français").click()
+        cy.contains("h2", "GESTIONNAIRE DE CONTENU")
+
+        // select english
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "English").click()
+        cy.contains("h2", "CONTENT MANAGER")
+    })
+})
