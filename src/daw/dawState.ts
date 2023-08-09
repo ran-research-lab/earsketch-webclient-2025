ec5c5c3af5c59cb3489475503ad4d0f375ee1453
Ian Clester <ijc@ijc8.me>
2023-08-09 16:00:49 -0400

    Don't enable metronome on solo (#592)
    Instead, respect metronome state as before.
    
    Runoff from #579, fixes GTCMT/earsketch#3215.

--- src/daw/dawState.ts
+++ src/daw/dawState.ts
@@ -240,8 +240,8 @@ export const getMuted = (tracks: Track[], soloMute: SoloMuteConfig, metronome: b
     const keys = Object.keys(tracks).map(x => +x)
     const soloed = keys.filter(key => soloMute[key] === "solo")
     if (soloed.length > 0) {
-        // Omit mix track and (if metronome is enabled) metronome track.
-        return keys.filter(key => !soloed.includes(key) && key !== 0)
+        // Omit mix track if metronome is enabled.
+        return keys.filter(key => !soloed.includes(key) && !(metronome && key === 0))
     } else {
         return [...keys.filter(key => soloMute[key] === "mute"), ...(metronome ? [] : [0])]
     }
