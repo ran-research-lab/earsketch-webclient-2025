72a2f86f95279d155960380e65a6b9b1bf19ab81
Luke Heerman <43624284+heerman@users.noreply.github.com>
2024-08-14 08:47:11 -0400

    Update competition details (#659)
    Adds details for the EarSketch Back to School Beats Competition.

--- src/app/CompetitionSubmission.tsx
+++ src/app/CompetitionSubmission.tsx
@@ -14,11 +14,11 @@ export const CompetitionSubmission = ({ name, shareID, close }: { name: string, 
                         <span>Competition Rules</span>
                     </div>
                     <ul className="list-disc text-left p-5 pl-10 space-y-1 bg-gray-100 dark:bg-gray-800">
-                        <li>Submission deadline is 11:59PM PST on June 21st, 2024</li>
-                        <li>Song must be between 30 seconds and 2 minutes, include 5 unique musical tracks, and be a remix including stems from more than one artist (MS/HS students must also include a for loop)</li>
+                        <li>Submission deadline is 11:59PM PST on September 30th, 2024</li>
+                        <li>Song must be between 30 seconds and 2 minutes, include 5 unique musical tracks, and be a remix including stems from more than one artist</li>
                         <li>A reflection describing song&apos;s message must be included with submission</li>
                         <li>Students can only submit one song for the competition</li>
-                        <li>Students can work alone or with one partner</li>
+                        <li>Students can work alone or in groups up to fours (prizes will be split among teams)</li>
                         <li>For more information, please visit the <a href="https://www.teachers.earsketch.org/compete" target="_blank" rel="noreferrer">competition website</a>.</li>
                     </ul>
                 </div>
