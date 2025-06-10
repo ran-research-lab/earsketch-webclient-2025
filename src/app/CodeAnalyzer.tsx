ran-research-lab/earsketch-webclient-2025

commit 8e235b4e276640d1e61830774bbf9adb1645a5fc

Migrate from webpack to vite (#687)
🏎️

Resolves GTCMT/earsketch#3401.
Relates to GTCMT/earsketch#3478.

---------

Co-authored-by: Stephen Garrett <manodrum@gmail.com>

src/app/CodeAnalyzer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useAppDispatch as useDispatch, useAppSelector as useSelector } from "../hooks"
 import { ModalContainer } from "./App"
 import { readFile } from "./Autograder"

src/audio/renderer.ts
@@ -1,5 +1,5 @@
 // Render DAW projects to audio files using an offline audio context.
-import pitchshiftWorkletURL from "pitchshiftWorklet"
+import pitchshiftWorkletURL from "@lib/pitchshift/worklet?url"
 import esconsole from "../esconsole"
 import { Clip, DAWData } from "common"
 import { OfflineAudioContext } from "./context"

src/types/global.d.ts
@@ -6,24 +6,30 @@ declare const URL_DOMAIN: string
 declare const URL_WEBSOCKET: string
 declare const SITE_BASE_URI: string
 declare const BUILD_NUM: string
-declare const FLAGS: any
+
+declare const ES_WEB_ANALYTICS: boolean
+declare const ES_WEB_SHOW_COMPETITION_SUBMIT: boolean
+declare const ES_WEB_SHOW_COMPETITION_BANNER: boolean
+declare const ES_WEB_SHOW_FEATURED_SOUNDS: boolean
+declare const ES_WEB_FEATURED_ARTISTS: string[]
+declare const ES_WEB_SHOW_CAI: boolean
+declare const ES_WEB_UPLOAD_CAI_HISTORY: boolean
+declare const ES_WEB_SHOW_CHAT: boolean
+declare const ES_WEB_SHOW_LOCALE_SWITCHER: boolean
 
 declare const difflib: any
-declare const diffview: any
-declare const droplet: any
 declare const Hilitor: any
 declare const JSZip: any
 declare const lamejs: any
-// NOTE: It looks like bringing in d3 types would require upgrading past d3 v3,
-// which is a nontrivial undertaking because of significant API changes.
-// (I spent some time of this and decided against it; we might just drop the d3 dependency anyway.)
-declare const d3: any
 declare const Kali: any
 
+declare module "d3"
+declare module "droplet"
 declare module "js-interpreter"
 declare module "chance"
+declare module "@lib/jsdifflib/diffview"
+declare module "@lib/jsdifflib/difflib"
 
-declare const createAudioMeter: (audioContext: AudioContext, clipLevel: number, averaging: number, clipLag: number) => AudioNode
 declare const Recorder: any
 
 declare module "@webscopeio/react-textarea-autocomplete"
@@ -43,7 +49,7 @@ declare module "*.png" {
     export default content
 }
 
-declare module "dsp" {
+declare module "@lib/dsp" {
     const DSP: any
     const FFT: any
     const WindowFunction: any

commit 51ed512c6158f3608b298a9a0433766708d288c6

Fix dependencies used by exporter (#715)
Fallout from #687; fixes GTCMT/earsketch#3485.

To prevent future breakage, see GTCMT/earsketch#3487.

src/app/CodeAnalyzer.tsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react"
+import { Tab } from "@headlessui/react"
+import JSZip from "jszip"
+
 import { useAppDispatch as useDispatch, useAppSelector as useSelector } from "../hooks"
 import { ModalContainer } from "./App"
 import { readFile } from "./Autograder"
@@ -10,7 +13,6 @@ import { loadScript } from "../browser/scriptsThunks"
 import { getStandardSounds } from "../browser/soundsThunks"
 import { selectLoggedIn } from "../user/userState"
 import { parseLanguage } from "../esutils"
-import { Tab } from "@headlessui/react"
 
 const FormatButton = ({ label, formatChange, inputType, value }: {
     label: string, formatChange: (v: InputType) => void, inputType: InputType, value: InputType

src/app/exporter.ts
@@ -1,6 +1,7 @@
 // Export a script as text, audio file, or zip full of audio files.
 // Also supports printing scripts and uploading to SoundCloud (which is perplexing because we have another moduled named "uploader").
 import i18n from "i18next"
+import JSZip from "jszip"
 
 import type { DAWData, Script } from "common"
 import esconsole from "../esconsole"

src/audio/renderer.ts
@@ -1,4 +1,6 @@
 // Render DAW projects to audio files using an offline audio context.
+import * as lame from "@breezystack/lamejs"
+
 import pitchshiftWorkletURL from "@lib/pitchshift/worklet?url"
 import esconsole from "../esconsole"
 import { Clip, DAWData } from "common"
@@ -53,7 +55,7 @@ export async function renderWav(result: DAWData) {
 // Render a result to mp3 for offline playback. Returns a Blob.
 export async function renderMp3(result: DAWData) {
     const buffer = await renderBuffer(result)
-    const mp3encoder = new lamejs.Mp3Encoder(2, 44100, 160)
+    const mp3encoder = new lame.Mp3Encoder(2, 44100, 160)
     const mp3Data = []
 
     const left = float32ToInt16(buffer.getChannelData(0))

src/types/global.d.ts
@@ -17,10 +17,6 @@ declare const ES_WEB_UPLOAD_CAI_HISTORY: boolean
 declare const ES_WEB_SHOW_CHAT: boolean
 declare const ES_WEB_SHOW_LOCALE_SWITCHER: boolean
 
-declare const difflib: any
-declare const Hilitor: any
-declare const JSZip: any
-declare const lamejs: any
 declare const Kali: any
 
 declare module "d3"