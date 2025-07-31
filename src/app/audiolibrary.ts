4d96128e151d7fc580afec9bd4f1a307c2c8cbd2
Luke Heerman <43624284+heerman@users.noreply.github.com>
2025-07-31 12:33:48 -0400

    Require credentials to list user audio (#723)
    For now calls to `/audio/user` are backwards compatible, meaning the deprecated query parameter `username` is still included, until the backend is updated.
    
    Sister PR to GTCMT/earsketch-api#92.

--- src/app/audiolibrary.ts
+++ src/app/audiolibrary.ts
@@ -2,6 +2,7 @@
 import ctx from "../audio/context"
 import { SoundEntity } from "common"
 import esconsole from "../esconsole"
+import { getAuth } from "../request"
 
 const STATIC_AUDIO_URL_DOMAIN = URL_DOMAIN === "https://api.ersktch.gatech.edu/EarSketchWS"
     ? "https://earsketch.gatech.edu/backend-static"
@@ -153,8 +154,8 @@ async function _getStandardSounds() {
 }
 
 export async function getUserSounds(username: string) {
-    const response = await fetch(URL_DOMAIN + "/audio/user?" + new URLSearchParams({ username }))
-    const sounds: SoundEntity[] = await response.json()
+    // The /audio/user depricated query parameter `username` is maintained, for now, until the backend is updated.
+    const sounds: SoundEntity[] = await getAuth("/audio/user", { username })
     // Populate cache with user sound metadata so that we don't fetch it again later via `getMetadata()`.
     for (const sound of sounds) {
         fixMetadata(sound, false)
